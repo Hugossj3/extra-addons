1dc8a80f08f609deb701635a725e9b50d6ce5f93
Hugossj3 <gdhugo66@gmail.com>
2024-02-16 13:42:19 +0100

    modificando videoclub.xml

--- videoclub_sge/models/videoclub.py
+++ videoclub_sge/models/videoclub.py
@@ -1,12 +1,11 @@
-from odoo import mobels, fields, api
+from odoo import models, fields, api
 
 class videoclub_pelis(models.Model):
     _name='videoclub.pelis'
     _description='Peliculas'
 
     titulo=fields.Char('Titulo',size=30, required=True,help='Nombre de la peli')
-    director=fields.Char('Director',size=30,required=False, help='Director de la peli'
-                         , default='')
+    director=fields.Char('Director',size=30,required=False, help='Director de la peli', default='')
     clasificacion=fields.Selection([
         ('TP','Todos los publicos'),
         ('men12','Menores de 12'),
